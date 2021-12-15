a02276ef110d25eb73d4d8c77b09a8563983db41
CeAlCuadrado <cealcuadrado@outlook.com>
2021-12-15 15:39:25 -0300

    Establecido layout básico

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  columns: number = 4;
 
-  constructor() { }
+  cards: any = [
+    { title: 'Proveer', quantity: 363, icon: 'fa-user' },
+    { title: 'Proveer devuelto', quantity: 15, icon: 'fa-circle' },
+    { title: 'Firmar', quantity: 8, icon: 'fa-home' },
+    { title: 'Firmar tramitar', quantity: 50, icon: 'fa-apple-alt' },
+    { title: 'Sentencia devuelta', quantity: 57, icon: 'fa-archive' },
+    { title: 'Crear tarea', quantity: 22, icon: 'fa-at' },
+    { title: 'Generar sentencia', quantity: 9, icon: 'fa-award' },
+    { title: 'Generar medida cautelar', quantity: 8, icon: 'fa-bahai' },
+    { title: 'Firmar sentencia', quantity: 8, icon: 'fa-baseball-bat' },
+    { title: 'Firmar medida cautelar', quantity: 8, icon: 'fa-bath' },
+    { title: 'Resolver competencia', quantity: 8, icon: 'fa-bomb' },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void { }
+
+  calculateColumns(): string {
+    return `col-lg-${12 / this.columns}`;
   }
-
 }
